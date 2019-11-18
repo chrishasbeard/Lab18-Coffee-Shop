9dba9ffa4e749dae188d799978b83f49e75a0d98
chrishasbeard <chrisholmes16@gmail.com>
2019-11-18 09:08:16 -0500

    lab 18 coffee shooop

---
+++ Lab18 CoffeeShop/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab18_CoffeeShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab18_CoffeeShop.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult AddCustomer()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DisplayCustomer(Customer newCustomer)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(newCustomer);
+            }
+            else
+            {
+                return View("AddCustomer");
+            }
+        }
+    }
+}
---
+++ Lab18 CoffeeShop/Models/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab18_CoffeeShop.Models
+{
+    public class Customer
+    {
+        [RegularExpression(@"[^a-z\s][a-z]{1,30}")]
+        public string FirstName { get; set; }
+        [RegularExpression(@"[^a-z\s][a-z]{1,30}")]
+        public string LastName { get; set; }  //add regular expressions to verify inputs
+       // [RegularExpression(@"^[A-Za-z0-9]{5,30}[@][a-zA-Z0-9]{5,10}[.][A-Za-z0-9]{2,3}")]
+        public string Email { get; set; }       // add a page for after 
+        //[RegularExpression(@"\d{3}-\d{3}-\d{4}")]
+        public int PhoneNumber { get; set; }
+        public string Password { get; set; }
+
+        public Customer(string firstName, string lastName, string email, int phoneNumber, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+
+        public Customer() { }
+    }
+}
